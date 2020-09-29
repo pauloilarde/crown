b958cb5099ef8990564367a66e2819fe1b027fed
Paulo Ilarde <pauloilarde@gmail.com>
2020-09-29 12:36:42 -0300

    Criando componente collection preview - etrutura básica

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
+
 import SHOP_DATA from './shop.data';
+
+import CollectionPreview from '../../components/collection-preview/collection-preview.component';
 
 class ShopPage extends Component {
     constructor(props){
@@ -11,7 +14,16 @@ class ShopPage extends Component {
     }
 
     render(){
-        return <div>Shop Page</div>
+        const { collections } = this.state;
+        return (
+            <div className="shop-page">
+                {
+                    collections.map(({ id, ...otherCollectionProps }) => (
+                        <CollectionPreview key={id} {...otherCollectionProps} />
+                    ))
+                }
+            </div>
+        )
     }
 }
 
